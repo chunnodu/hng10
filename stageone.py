c473fb0da19ad1e067c4e9170279a4f54918cf24
em4mario <chuknnodu@gmail.com>
2023-09-10 21:57:52 -0400

    tested and trusted

--- stageone.py
+++ stageone.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 
-import json
-
 import datetime
 import time
 
 current_date = datetime.datetime.now()
 current_date_utc = datetime.datetime.now(datetime.timezone.utc)
-utc_time = current_date_utc.strftime("%Y-%m-%dT%H:%M:%SZ")
-
+utc_time =  f'{current_date_utc.strftime("%Y-%m-%dT%H:%M:%SZ")}'
 day = current_date.strftime("%A")
 
 app = FastAPI()
@@ -17,10 +14,6 @@ app = FastAPI()
 
 def endpoint(slack_name, track):
     statuscode = 200
-    data = {
-        "slack_name":slack_name, "track":track, "current_day":day, "utc_time":utc_time,
-        "github_file_url":'https://github.com/chunnodu/hng10/blob/main/stageone.py', 
-        "github_repo_url":'https://github.com/chunnodu/hng10', "status_code": statuscode
-        }
+    data = {"slack_name":slack_name, "track":track, "current_day":day, "utc_time": utc_time,"github_file_url":'https://github.com/chunnodu/hng10/blob/main/stageone.py', "github_repo_url":'https://github.com/chunnodu/hng10', "status_code": statuscode}
     return data
 
