7ad79c0d2e0e2bf05ec43c80ba62b40576a86877
em4mario <chuknnodu@gmail.com>
2023-09-10 23:03:56 -0400

    fixed utc again

--- stageone.py
+++ stageone.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
 from datetime import datetime, timezone
 
+import json
 import time
-
-current_date = datetime.now()
-current_date_utc = datetime.now(timezone.utc)
-utc_time =  f'{current_date_utc.strftime("%Y-%m-%dT%H:%M:%SZ")}'
-day = current_date.strftime("%A")
 
 app = FastAPI()
 
@@ -14,6 +10,23 @@ app = FastAPI()
 
 def endpoint(slack_name, track):
     statuscode = 200
-    data = {"slack_name":slack_name, "track":track, "current_day":day, "utc_time": utc_time,"github_file_url":'https://github.com/chunnodu/hng10/blob/main/stageone.py', "github_repo_url":'https://github.com/chunnodu/hng10', "status_code": statuscode}
-    return data
+    current_date = datetime.now()
+    current_date_utc = datetime.now(timezone.utc)
+    utc_time =  f'{current_date_utc.strftime("%Y-%m-%dT%H:%M:%SZ")}'
+    day = current_date.strftime("%A")
+
+    return {
+        "slack_name":slack_name, 
+        "track":track, 
+        "current_day":day, 
+        "utc_time": utc_time,
+        "github_file_url":'https://github.com/chunnodu/hng10/blob/main/stageone.py', 
+        "github_repo_url":'https://github.com/chunnodu/hng10', 
+        "status_code": statuscode
+        }
+    
+
+
+    
+  
 
